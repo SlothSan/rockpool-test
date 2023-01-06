1a729ed5a4b7fc5aaa50e0f37bf70634ee8ac8d2
Mike Oram <Mikeoram01@gmail.com>
2023-01-06 13:23:48 +0000

    removed boilerplate and add components to the app.component.html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { ThankyouComponent } from './thankyou/thankyou.component';
+//Import the form module for later use. 
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { ThankyouComponent } from './thankyou/thankyou.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
