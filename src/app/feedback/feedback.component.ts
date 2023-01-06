37ba00b20b701a28b700bf8ab7da7057354486db
Mike Oram <Mikeoram01@gmail.com>
2023-01-06 14:25:21 +0000

    added some really basic styling

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Form, FormControl, FormGroup } from '@angular/forms';
+import { Form, FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 
 @Component({
@@ -21,8 +21,8 @@ export class FeedbackComponent {
       })
     }
 
-    onFormSubmit() { 
-      console.log(this.feedbackFormGroup.value);  
+    onFormSubmit(formData: any) { 
+      console.log(formData);  
     } 
    
 }
