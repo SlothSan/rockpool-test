9f119b2fc3cc195c76af3fddd01ddb187548ab3c
Mike Oram <Mikeoram01@gmail.com>
2023-01-06 13:19:07 +0000

    created the componenents needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { ThankyouComponent } from './thankyou/thankyou.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackComponent,
+    ThankyouComponent
   ],
   imports: [
     BrowserModule,
